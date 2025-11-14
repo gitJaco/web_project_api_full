154698e10822328d985d6df6316121a965741578
Jacobo <jacopeth88@gmail.com>
2025-11-14 14:29:58 -0600

    Ajustar cors

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ var cors = require('cors');
 
 
 const corsOptions = {
-  origin: 'https://api.jacopeth.twilightparadox.com',
+  origin: 'https://jacopeth.twilightparadox.com',
   methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
