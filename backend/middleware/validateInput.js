2cd60fb50b852ba822db29407c2bc87e7944aa74
Jacobo <jacopeth88@gmail.com>
2025-11-15 10:05:31 -0600

    Corregir module exports

--- backend/middleware/validateInput.js
+++ backend/middleware/validateInput.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 
-module.export.validateInput = celebrate({
+module.exports.validateInput = celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().min(2).max(30),
       password: Joi.string().required().min(2),
